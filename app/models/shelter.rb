ad77354ef08350c12c4af2c767dfb59af0b4de70
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-23 17:57:27 -0400

    Add .order(:name) to pending #shelters_with_pending_apps to alphabetize

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -21,7 +21,7 @@ class Shelter < ApplicationRecord
   end
 
   def self.shelters_with_pending_apps
-    joins( pets: :applications).where("applications.status = 'Pending'")
+    joins( pets: :applications).where("applications.status = 'Pending'").order(:name)
   end
 
   def self.name_and_address(shelter_id)
