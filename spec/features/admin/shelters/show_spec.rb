e56d61973e7ae8fcdfaf6d2fa3a5300621ad8cdb
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-05-23 18:51:40 -0400

    Add the feature test for adoptable pet count on admin shelter show page

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -8,7 +8,6 @@ require 'rails_helper'
 describe 'admin shelter show page', type: :feature do
   let!(:shelter_1) {Shelter.create!(name: 'Aurora shelter', street_address: '15 Fluff Lane', state: 'CO', zip_code: 80152, city: 'Aurora', foster_program: false, rank: 9)}
   let!(:shelter_2) {Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)}
-  # let!(:shelter_3) {Shelter.create!(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)}
 
   it "shows the shelter's name and full address" do
     visit "/admin/shelters/#{shelter_1.id}"
@@ -19,6 +18,16 @@ describe 'admin shelter show page', type: :feature do
     expect(page).to have_content(80152)
     expect(page).to have_content('Aurora')
     expect(page).to_not have_content('RGV animal shelter')
+  end
+
+  it "displays the number of adoptable pets at the shelter" do
+    Pet.create(adoptable: false, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter_1.id)
+    Pet.create!(name: "Zucchini", breed: 'weenie dog', age: 7, adoptable: true, shelter_id: shelter_1.id)
+    Pet.create!(name: "Tater Tot", breed: 'french bulldog', age: 5, adoptable: true, shelter_id: shelter_1.id)
+
+    visit "/admin/shelters/#{shelter_1.id}"
+    expect(page).to have_content("Number of Adoptable Pets: 2")
+
   end
 
 end
