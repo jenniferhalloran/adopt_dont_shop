bb3639bf8e5cddbf7af938ec355afe0c3add5394
Stephen Wilkens <stephenwilkens@gmail.com>
2022-05-24 18:46:48 -0600

    Add all pets accepted changes application status to accepted functionality and one pet is rejected changes application status to rejected functionality

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,11 +1,11 @@
 class Admin::ApplicationsController < ApplicationController
 
-    def show
-        @application = Application.find(params[:id])
-        if @application.approved?
-            @application.update(status: "Approved")
-        elsif @application.rejected?
-            @application.update(status: "Rejected")
-        end
+  def show
+    @application = Application.find(params[:id])
+    if @application.approved?
+      @application.update(status: "Approved")
+    elsif @application.rejected?
+      @application.update(status: "Rejected")
     end
+  end
 end
